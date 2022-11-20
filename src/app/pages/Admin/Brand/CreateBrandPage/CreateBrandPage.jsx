d27a0e6cf8b7a5716cfccbbc4db8259448330de5
anandu.reghu <anandu.reghu@qburst.com>
2022-11-20 20:31:25 +0530

    implemented image upload

--- src/app/pages/Admin/Brand/CreateBrandPage/CreateBrandPage.jsx
+++ src/app/pages/Admin/Brand/CreateBrandPage/CreateBrandPage.jsx
@@ -1,9 +1,19 @@
+import { async } from "@firebase/util";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import React, { useState } from "react";
+import { useEffect } from "react";
+import { v4 } from "uuid";
 import { createBrand } from "../../../../db/brands";
+import { getCategories } from "../../../../db/category";
+import { storage } from "../../../../service/firebase/firebase";
 import "./CreateBrandPage.scss";
 
 const CreateBrandPage = () => {
   const [brand, setBrand] = useState(emptyBrand);
+  const [categories, setCategories] = useState([]);
+  const [logo, setLogo] = useState("");
+  const [logoUrl, setLogoUrl] = useState("");
+
   const handleChange = (e) => {
     const key = e.target.name;
     let value = e.target.value;
@@ -16,17 +26,47 @@ const CreateBrandPage = () => {
     });
   };
 
+  const upoloadLogo = async () => {
+    if (logo == null) return false;
+    let uploaded = false;
+    const logoRef = ref(storage, `images/${logo.name + v4()}`);
+    await uploadBytes(logoRef, logo)
+      .then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
+          setLogoUrl(url);
+          console.log(url);
+          uploaded = true;
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        uploaded = false;
+      });
+    return uploaded;
+  };
+
   const handleBrandCreate = () => {
-    const isCreated = validateBrand() && createBrand({
-      ...brand,
-      joined: new Date(),
+    upoloadLogo().then(() => {
+      const isCreated =
+        validateBrand() &&
+        createBrand({
+          ...brand,
+          imageUrl: logoUrl,
+          joined: new Date(),
+        });
+      if (isCreated) {
+        setBrand(emptyBrand);
+      }
     });
-    if (isCreated) {
-      setBrand(emptyBrand);
-    }
   };
 
   const validateBrand = () => brand.name.length > 2;
+
+  useEffect(() => {
+    getCategories().then((data) => {
+      setCategories(data);
+    });
+  }, []);
 
   return (
     <div className="brand__create">
@@ -41,6 +81,21 @@ const CreateBrandPage = () => {
           name="name"
           onChange={handleChange}
           value={brand.name}
+        />
+        <select name="category" placeholder="Category" onChange={handleChange}>
+          <option value="0" disabled>
+            --select-category--
+          </option>
+          {categories.map((category) => {
+            return <option value={category.id}>{category.name}</option>;
+          })}
+        </select>
+        <label htmlFor="logo">Choose logo for brand</label>
+        <input
+          type="file"
+          name="logo"
+          id="logo"
+          onChange={(e) => setLogo(e.target.files[0])}
         />
         <textarea
           type="text"
